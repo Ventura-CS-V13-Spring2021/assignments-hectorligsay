e0cff997d848d65805d4dd271ea497539ae5e7db
hectorligsay <hligsay@aol.com>
2021-02-23 23:17:54 +0000

    Uploaded the correct Assignment 2-1 file for the greatest value program.

--- assignment2-1.cpp
+++ assignment2-1.cpp
@@ -1,22 +1,25 @@
-// This program will convert Celsius temperatures to Fahrenheit temperatures
+// This program will find the greatest value out of the 3 numbers input form user 
+
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    // This will get user's input for temp in Fahrenheit and assign it to a variable
-    
-    float Celsius, fahrenheit;
+    int num1, num2, num3;
+    int current_max;
 
-    cout << "Hello! What is the temperature by Celcius?: ";
-    cin >> Celsius;
-    cout << endl;
+    cout << "Enter 3 random numbers: ";
+    cin >> num1 >> num2 >> num3;
 
-    // This will calculate celsius amount to fahrenheit
-    fahrenheit = (9.0 / 5.0) * Celsius + 32;
+    current_max = num1;
 
-    // This is return the value in fahrenheit to the terminal
-    cout <<"Celcius " << Celsius<< " is " <<fahrenheit <<" Farentheit.\n";
+    if ( current_max < num2 )
+        current_max = num2;
+    if ( current_max < num3 )
+        current_max = num3;
+
+    cout << "The max value is " << current_max << endl;
 
     return 0;
 
