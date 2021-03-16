b9a10cee1b3281c319424f7a8fe45cca5de9b22a
hectorligsay <hligsay@aol.com>
2021-03-16 04:47:39 +0000

    10 min commin mark.

---
+++ quiz.2.2.cpp/question1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+//This program enters 5 random numbers and will find a number greater than the previous number
+
+int main()
+{
+  int num1, num2, num3;
+
+}
