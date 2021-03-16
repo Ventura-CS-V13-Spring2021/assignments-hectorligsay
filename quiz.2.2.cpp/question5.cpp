8054a97863c8c39c88531acacebb74bc245168dd
hectorligsay <hligsay@aol.com>
2021-03-16 05:46:18 +0000

    Fixing prime program. Not running correctly in VScode, but is running correctly in Repl.it??

--- quiz.2.2.cpp/question5.cpp
+++ quiz.2.2.cpp/question5.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+// this program finds the prime numbers in the given range
+// Requirement:
+// input (2 positive integers)
+// output (The list of prime number list in given range)
+
 
 int main()
 {
-    int i;
-    int range1, range2;
+    int range1 , range2 ;
+    int i, p;
 
-    range1 = 2;
-    range2 = 10;
- 
-    for (range1; range1 <= range2; range1++) 
+
+    do{
+        cout << "Enter the range (first number must be smaller than second): " << endl;
+        cin >> range1 >> range2;
+    } while (range1 >= range2 || range1 <2 || range2 <0) ;
+    
+    for ( p = range1; p <= range2; p++ )
     {
-        int small = 0;
-        for (i = 2; i <= (range1/2); i++) 
-        {
-            if (range1 % i == 0) 
-            {
-                small ++ ; 
+        if ( p== 0 || p== 1)
+            continue;
+        for (i = 2; i <= (p/ 2); i ++)
+            if (p % i == 0)
                 break;
-            }
-        }
 
-        if (  (small == 0 ) && range1!= 1) { cout << range1<< " is prime\n"; }
-        else { cout << range1 << " is not prime\n"; }
+        if ( i > (p/2) )
+            cout << p << " is a prime number." << endl;
+
     }
+    return 0;
 }
