3ee395fa09f94762aec15c9d872ba83c59449fde
hectorligsay <hligsay@aol.com>
2021-03-16 04:59:57 +0000

    Commit Check up.

--- quiz.2.2.cpp/question1.cpp
+++ quiz.2.2.cpp/question1.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main()
 {
-  int num1, num2, num3;
+  int num1, num2, num3,num4, num5;
+
+  num1 = rand() % 100;
+  num2 = rand() % 100;
+  num3 = rand() % 100;
+  num4 = rand() % 100;
+  num5 = rand() % 100;
+
+  cout << num1 << num2 << num3 << num4 << num5;
+
 
 }
