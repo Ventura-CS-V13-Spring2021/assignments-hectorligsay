546edaaf5db3263e086e69d5747e21e8181e0ec2
hectorligsay <hligsay@aol.com>
2021-03-16 05:12:33 +0000

    Commit Check up

--- quiz.2.2.cpp/question1.cpp
+++ quiz.2.2.cpp/question1.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>
 using namespace std;
 //This program enters 5 random numbers and will find a number greater than the previous number
 
 int main()
 {
   int num1, num2, num3,num4, num5;
+  int max, nxt;
 
-  num1 = rand() % 100;
-  num2 = rand() % 100;
-  num3 = rand() % 100;
-  num4 = rand() % 100;
-  num5 = rand() % 100;
+  num1 = arc4random() % 100;
+  num2 = arc4random() % 100;
+  num3 = arc4random() % 100;
+  num4 = arc4random() % 100;
+  num5 = arc4random() % 100;
 
-  cout << num1 << num2 << num3 << num4 << num5;
+  if ( max < num2 )
+    max = num2;
+  if ( max < num3 )
+    max = num3;  
+  if ( max < num4 )
+    max = num4;  
+  if ( max < num5 )
+    max = num5;   
+
+  cout << max << endl;
 
 
 }
