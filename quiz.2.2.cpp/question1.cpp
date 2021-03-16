646d9eb08db07488041e9501dca77b6d9839f4e4
hectorligsay <hligsay@aol.com>
2021-03-16 07:16:41 +0000

    Finishing Question 3

--- quiz.2.2.cpp/question1.cpp
+++ quiz.2.2.cpp/question1.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int main()
 {
   int num1, num2, num3,num4, num5;
-  int max, nxt;
+  int max, max2;
+  int i;
 
   num1 = arc4random() % 100;
   num2 = arc4random() % 100;
@@ -15,16 +16,25 @@ int main()
   num4 = arc4random() % 100;
   num5 = arc4random() % 100;
 
-  if ( max < num2 )
-    max = num2;
-  if ( max < num3 )
-    max = num3;  
-  if ( max < num4 )
-    max = num4;  
-  if ( max < num5 )
-    max = num5;   
+  if ((num1 < 0) || (num2 < 0) || (num3 < 0) || (num4 < 0) || (num5 < 0))
+        {cout << "This is not a valid entry";
+        }
+  else  
+    if ( max < num2 )
+      max = num2;
+    else if ( max < num3 )
+      max = num3;  
+    else if ( max < num4 )
+      max = num4;  
+    else if ( max < num5 )
+      max = num5;
+    
+  if (max = num2)
+    
+
 
   cout << max << endl;
 
 
 }
+
