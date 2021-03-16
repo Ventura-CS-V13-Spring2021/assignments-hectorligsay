d80cf89352c79c7ab4507a72190d4af11a175290
hectorligsay <hligsay@aol.com>
2021-03-16 05:49:19 +0000

    Fixed question 5 again.

--- quiz.2.2.cpp/question5.cpp
+++ quiz.2.2.cpp/question5.cpp
@@ -11,25 +11,21 @@ using namespace std;
 int main()
 {
     int range1 , range2 ;
-    int i, p;
+    int p, i;
 
-
-    do{
         cout << "Enter the range (first number must be smaller than second): " << endl;
         cin >> range1 >> range2;
-    } while (range1 >= range2 || range1 <2 || range2 <0) ;
-    
+
     for ( p = range1; p <= range2; p++ )
     {
-        if ( p== 0 || p== 1)
-            continue;
-        for (i = 2; i <= (p/ 2); i ++)
+        for (i = 2; i <= (p/2); i ++)
             if (p % i == 0)
                 break;
 
-        if ( i > (p/2) )
+        if ( i >= (p/2) )
             cout << p << " is a prime number." << endl;
-
+        else
+            cout << p << " is not prime" << endl;
     }
     return 0;
 }
