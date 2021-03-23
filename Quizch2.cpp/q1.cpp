a828f7e9a32616f048775cd5b6a8106555456990
hectorligsay <hligsay@aol.com>
2021-03-23 02:32:56 +0000

    Finished q1.

--- Quizch2.cpp/q1.cpp
+++ Quizch2.cpp/q1.cpp
@@ -44,8 +44,80 @@ int main()
     }
   
   // Finds the 2nd greatest number
-  if (max == num1)
-    if num2 < num3
 
+  if (max == num1)            //Removes num1 if it's the max 
+    {
+      if (num2 < num3)
+      {
+        nextmax = num3;
+      }
+        else if (num2 > num3)
+        nextmax = num2;
+      if (nextmax < num4)
+        nextmax = num4;
+      if (nextmax < num5)
+        nextmax = num5;
+    }
+
+  if (max == num2)            //Removes num2 if it's the max 
+    {
+      if (num1 < num3)
+      {
+        nextmax = num3;
+      }
+        else if (num1 > num3)
+        nextmax = num1;
+      if (nextmax < num4)
+        nextmax = num4;
+      if (nextmax < num5)
+        nextmax = num5;
+    }
+
+  if (max == num3)            //Removes num3 if it's the max 
+    {
+      if (num1 < num2)
+      {
+        nextmax = num2;
+      }
+        else if (num1 > num2)
+        nextmax = num1;
+      if (nextmax < num4)
+        nextmax = num4;
+      if (nextmax < num5)
+        nextmax = num5;
+    }
+
+  if (max == num4)            //Removes num4 if it's the max 
+    {
+      if (num1 < num2)
+      {
+        nextmax = num2;
+      }
+        else if (num1 > num2)
+        nextmax = num1;
+      if (nextmax < num3)
+        nextmax = num3;
+      if (nextmax < num5)
+        nextmax = num5;
+    }
+
+  if (max == num5)            //Removes num5 if it's the max 
+    {
+      if (num1 < num2)
+      {
+        nextmax = num2;
+      }
+        else if (num1 > num2)
+        nextmax = num1;
+      if (nextmax < num3)
+        nextmax = num3;
+      if (nextmax < num4)
+        nextmax = num4;
+    }
+
+  cout << "The list of random numbers are the following: " << endl;
+  cout << num1 << ' ' << num2 << ' ' << num3 << ' ' << num4 << ' ' << num5 << ' '<< endl;
+  cout << "The max number is " << max << '.' <<endl;
+  cout << "The next highest number is " << nextmax << '.' << endl;
 
 }
