5311e67b610f93178c6614ff6c652d6a17b5413b
hectorligsay <>
2021-04-19 23:14:45 +0000

    Almost done with question 2

--- midterm/midquest2.cpp
+++ midterm/midquest2.cpp
@@ -5,11 +5,12 @@
 using namespace std;
 
 int getRdnum(void);
-int isGreater(int &, int &);
+int isGreater(int , int );
 
 int main()
 {
   int rdnum, flag, prec;
+  string answer;
   int N = 10;
   ofstream    rdfile;  
 
@@ -17,30 +18,37 @@ int main()
   srand(time(NULL)); 
 
   // Generates random numbers
-  for (int i = 0; i<N ; i++)
+
+  do
   {
-    rdnum = getRdnum();
-    cout << rdnum << endl;
-
-    if ( i == 0 )
+    for (int i = 0; i<N ; i++)
     {
+      rdnum = getRdnum();
+      cout << rdnum << endl;
+
+      if ( i == 0 )
+      {
+        prec = rdnum;
+      }
+
+      
+      if (isGreater(rdnum, prec) == 1)
+      {
+        rdfile << rdnum <<  " is greater " << endl;
+      }
       prec = rdnum;
-    }
 
-    
-    if (isGreater(rdnum, prec) == 1)
-    {
-      rdfile << rdnum << endl;
     }
-    prec = rdnum;
+    cout << "Would you like to go again? " << endl;
+    cin >> answer;
+  } while (answer == "y" || answer == "Y");
+  
+  cout << "Thank you for playing! " << endl;
 
-  }
 
   rdfile.close();
 
 }
-
-
 
 
 int getRdnum(void)
@@ -49,7 +57,7 @@ int getRdnum(void)
   rdnum =(rand() % 51);
 }
 
-int isGreater(int &n, int &prec)
+int isGreater(int n, int prec)
 {
   if (prec < n)
   {
@@ -59,6 +67,7 @@ int isGreater(int &n, int &prec)
   {
     return 0;
   }
+  
 }
 
 
