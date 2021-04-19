7ee8e9f157d261f9dda9ca475f5ce6f46b2ce678
hectorligsay <>
2021-04-19 04:26:15 +0000

    10 min commit checkpoint

--- midterm/midquest2.cpp
+++ midterm/midquest2.cpp
@@ -1,4 +1,44 @@
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
 using namespace std;
+
+//int getRdnum(void);
+//int isGreater(int n);
+
+
+int main()
+{
+
+  int rdnum, prec;
+  int N = 10;
+  ofstream    rdfile;                                         //ofstream will WRITE the file only
+
+  rdfile.open("rdnum.txt");                                   
+                           
+
+  srand(time(NULL)); 
+  
+  for (int i = 0; i<N ; i++)
+  {
+    rdnum =(rand() % 51);
+
+    cout << rdnum << endl;
+
+    if ( i == 0 )
+    {
+      prec = rdnum;
+      continue;
+    }
+
+    if (prec < rdnum)
+    {
+      rdfile << rdnum << endl;
+    }
+    prec = rdnum;
+  }
+
+  rdfile.close();
+
+}
