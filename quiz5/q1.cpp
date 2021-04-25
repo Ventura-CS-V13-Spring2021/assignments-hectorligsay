aa746bea014b3e302a64d095369d096a4ade78aa
hectorligsay <>
2021-04-25 15:09:26 +0000

    Created quiz5 folder and starting question 1

---
+++ quiz5/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  const int   SIZE = 10;
+  int     numbers[SIZE] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
+  int     diff[SIZE];
+  int   sum = 0;
+
+  for (i)
+}
