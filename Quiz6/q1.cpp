5be5351f0478bb33c10ae6f3fbbd758c061c3249
hectorligsay <>
2021-05-02 07:32:40 +0000

    Still attempting question 1

--- Quiz6/q1.cpp
+++ Quiz6/q1.cpp
@@ -14,50 +14,75 @@ class IntegerArray
     int getLength(void) const;  // gets current length
     void sortArray(int flag); // if flag = 0, ascend else descend
     void fillUp(void);      // fills randnums 0-99
-    void getPrimenumber(void) const;
-    void printAll(void) const;
+    // void getPrimenumber(void) const;
+    // void printAll(void) const;
 };
 
-int IntegerArray::getLength(void) const     
-{
+int IntegerArray::getLength(void) const{
   return length;
 }
 
-void sortArray(int flag)
+void IntegerArray::sortArray(int flag){
+  flag == 0;
+  if (flag == 0){ 
+    for (int i = 0; i < 10; i++){ 
+      cout << numbers[i] << "\t";
+      length ++;
+     }
+    qsort(numbers, 9);
+    for (int i = 0; i < 10; i++){ 
+      cout << numbers[i] << "\t";
+    } 
+  }
+
+  else if (flag == 1){
+    for (int i = 0; i < 10; i++){
+      cout << numbers[i] << "\t";
+      length++;
+    }
+    qsort(numbers, 9;
+    for (int i = 0; i < 10; i++){ 
+      cout << numbers[i] << "\t";
+    }
+  }
+}
+
+void IntegerArray::fillUp(void)
 {
-  if (flag == 0)
-    for (int i = 0; i < 10; i++)
-      
+  srand(time(0));
+  for (int i =0; i < length; i++){
+    numbers[i] = rand() % 100;
+  }
 }
 
 int main()
 
 {
 
-    IntegerArray N;
+  IntegerArray N;
 
 
-    N.fillUp();
+  // N.fillUp();
 
-    N.printAll();
+  // N.printAll();
 
-    cout << "Current length is " << N.getLength() << endl;
-
-
-    cout << "After sorting with ascending order : \n ";
-
-    N.sortArray(0);
-
-    N.printAll();
+  cout << "Current length is " << N.getLength() << endl;
 
 
-    cout << "After sorting with descending order: \n ";
+  cout << "After sorting with ascending order : \n ";
 
-    N.sortArray(1);
+  N.sortArray(0);
 
-    N.printAll();
+  // //N.printAll();
 
 
-    cout << "The number of prime numbers " << N.getPrimenumbers() << endl;
+  // cout << "After sorting with descending order: \n ";
+
+  // N.sortArray(1);
+
+  // N.printAll();
+
+
+  // cout << "The number of prime numbers " << N.getPrimenumbers() << endl;
 
 }
