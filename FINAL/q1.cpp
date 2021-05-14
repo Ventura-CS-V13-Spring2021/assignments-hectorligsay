18f282af275e74dd025d1f0942a424e88cfa9266
hectorligsay <>
2021-05-14 02:33:17 +0000

    Still on q1

--- FINAL/q1.cpp
+++ FINAL/q1.cpp
@@ -8,25 +8,39 @@ using namespace std;
 class integerArray{
 
 private: 
-  int size[6];
+  int numbers[6];
   int length;
 
 public:
+  int getLength(void) const;
+  void sortArray(void);
   void createArray(void);
+};
+
+int IntegerArray::getLength(void) const{
+    return length;
 }
 
-int integerArray::createArray(void){
-  int size = 6;       //size of array
+void sortArray(void){       //ASCENDS
+  //To ascend the order
+  sort(numbers, numbers + length);
+}
 
-  for (int i=0; i< size; i++){  
+void integerArray::createArray(void){     //creates array w/ 6 rand elements
+  srand(time(0));     //initializes randoms 
+  for (int i=0; i< length; i++){  
     numbers[i] = rand() % 100;
+    length++;
   }
-
-void sortArray(){
-
 }
 
-int getNumDiv(int i){
+void printAll(void) const{
+    for (int i=0; i < length; i++)
+        cout << numbers[i] << "\t";
+    cout << endl;
+}
+
+int getNumDiv(int i){                 //Gets nums divided
 
 }
 
