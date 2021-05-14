dd9833c20f348a72a7132a35a6a3ee4a95ef30b6
hectorligsay <>
2021-05-14 02:26:31 +0000

    Working qith q1 still

--- FINAL/q1.cpp
+++ FINAL/q1.cpp
@@ -1,2 +1,35 @@
 #include <iostream>
+#include <cmath>
+#include<algorithm>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
+class integerArray{
+
+private: 
+  int size[6];
+  int length;
+
+public:
+  void createArray(void);
+}
+
+int integerArray::createArray(void){
+  int size = 6;       //size of array
+
+  for (int i=0; i< size; i++){  
+    numbers[i] = rand() % 100;
+  }
+
+void sortArray(){
+
+}
+
+int getNumDiv(int i){
+
+}
+
+int main(){
+  
+}
